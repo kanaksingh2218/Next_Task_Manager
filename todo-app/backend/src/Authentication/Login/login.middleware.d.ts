1b3d5cf918c9b1a07c1ebc5203aabbfa66e960f8
kanaksingh2218 <kanaksingh2218@gmail.com>
2026-01-17 12:04:09 +0530

    Clean up compiled backend files and update gitignore

--- todo-app/backend/src/Authentication/Login/login.middleware.d.ts
+++
@@ -1,3 +0,0 @@
-import { Request, Response, NextFunction } from 'express';
-export declare const validateLogin: (req: Request, res: Response, next: NextFunction) => Response<any, Record<string, any>> | undefined;
-//# sourceMappingURL=login.middleware.d.ts.map
